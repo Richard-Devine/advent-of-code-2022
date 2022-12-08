26263f908c22bc0104ea6cd4d6853c103607eeec
Richard Devine <richard_devine@outlook.com>
2022-12-08 11:08:56 +0000

    day 1 and 2

--- day-2/src.js
+++ day-2/src.js
@@ -5,6 +5,7 @@ const rockPaperScissorsResults = (array) => {
     let results = []
     for(let i = 0; i < data.length; i++){
         let round = data[i].split("")
+        round.splice(1,1)
         let a
         let b
         let c
@@ -19,7 +20,7 @@ const rockPaperScissorsResults = (array) => {
                 a = 3
                 break;
         }
-        switch (round[2]){
+        switch (round[1]){
             case "X":
                 b = 1
                 break;
@@ -30,21 +31,18 @@ const rockPaperScissorsResults = (array) => {
                 b = 3
                 break;
         }
-        if(a === 1 && b === 1) c = 3
-        if(a === 1 && b === 2) c = 4
-        if(a === 1 && b === 3) c = 8
-
-        if(a === 2 && b === 1) c = 1
-        if(a === 2 && b === 2) c = 5
-        if(a === 2 && b === 3) c = 9
-
-        if(a === 3 && b === 1) c = 2
-        if(a === 3 && b === 2) c = 6
-        if(a === 3 && b === 3) c = 7
+        if(a === 1) {
+            b === 1 ? c = 3 : b === 2 ? c = 4 : c = 8
+        }
+        if(a === 2) {
+            b === 1 ? c = 1 : b === 2 ? c = 5 : c = 9
+        }
+        if(a === 3){
+            b === 1 ? c = 2 : b === 2 ? c = 6 : c = 7
+        }
 
         results.push(c)
     }
-    console.log(results)
     let total = 0
     for(const value of results){
         total += value
